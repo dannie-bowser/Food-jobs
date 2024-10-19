9b704e5eb66c8a35cf3fd7b5233dd205e3d88bfa
Mesogboriwon Daniel <dhanel0u0@gmail.com>
2024-10-18 18:22:51 -0700

    added json file

--- Food-jobs/src/App.jsx
+++ Food-jobs/src/App.jsx
@@ -13,9 +13,9 @@ const App = () => {
     {/* <!-- Browse Jobs --> */}
     <section class="bg-blue-50 px-4 py-10">
       <div class="container-xl lg:container m-auto">
-        <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
-          Browse Jobs
-        </h2>
+          <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+             Browse Jobs
+          </h2>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
           {/* <!-- Job Listing 1 --> */}
           <div class="bg-white rounded-xl shadow-md relative">
