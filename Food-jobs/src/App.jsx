c40358794541c9c5297633c9db31c9f180f03f81
Mesogboriwon Daniel <dhanel0u0@gmail.com>
2024-10-18 20:58:01 -0700

    statred react routing

--- Food-jobs/src/App.jsx
+++ Food-jobs/src/App.jsx
@@ -1,3 +1,9 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
@@ -6,18 +12,12 @@ import JobListings from './components/JobListings'
 import Viewjobs from './components/ViewAllJobs'
 
 
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCard />
-      <JobListings />
-       <Viewjobs />
-    
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+);
 
-    </>
-  )
+const App = () => {
+  return <RouterProvider router={router}/>
 }
 
 export default App
