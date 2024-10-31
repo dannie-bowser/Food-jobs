b8bc58a73281c356ab9a0d6ffc4bd97a40b23b5f
Mesogboriwon Daniel <dhanel0u0@gmail.com>
2024-10-31 06:36:06 -0700

    fixed bug

--- Food-jobs/src/pages/Jobpage.jsx
+++ Food-jobs/src/pages/Jobpage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+
+// import { useState,useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+
 
 const Jobpage = () => {
-  return (
-    <div>Jobpage</div>
-  )
+   
+   const { id } = useParams();
+  //  const [job, setJob] = useState(null);
+  //  const  [loading, setLoading] = useState(true);
+
+  //  useEffect(() => { 
+  //      const fetchJobs = async () => {
+  //       try {
+          
+  //         const res = await fetch( `/api/jobs/${id}` );
+  //         const data = await res.json();
+  //         console.log(data)
+  //         setJob(data);
+
+  //       } catch (error) {
+
+  //         console.log('Error fetching data', error);
+
+  //       } finally {
+          
+  //         setLoading(false);
+
+  //       }
+  //      }
+  //      fetchJobs();
+  //  }, [])
+
+   return (
+    loading ? <Spinner /> : <h2>{ job.title }</h2>
+   )
 }
+
+  const jobLoader = async (params) => {
+    
+  }
 
 export default Jobpage
